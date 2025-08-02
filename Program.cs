517c8c926ea4a5beb265673555758e401b90f818
shubham yadav <shubham38611@gmail.com>
2025-08-02 19:20:30 +0530

    Second commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,17 @@
     static void Main(string[] args)
     {
         System.Console.WriteLine("Hello, World!");
+        Greet();
         System.Console.ReadLine();
+    }
+
+    static void Greet()
+    {
+        System.Console.WriteLine("Greetings from the Greet method!");
+    }
+
+    static void AnotherMethod()
+    {
+        System.Console.WriteLine("This is another method.");
     }
 }
